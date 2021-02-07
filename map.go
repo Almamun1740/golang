b101bdaf428e7808f1502d679d421f64543e8072
Al Mamun Khan <67066348+Almamun1740@users.noreply.github.com>
2021-02-07 18:41:59 +0600

    Add files via upload

---
+++ map.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+func main(){
+ 
+x := make(map[string]string)
+x["name"] = "Mamun"
+//x["height"] = "5.8"
+//x["address"] = "Pabna"
+
+//delete(x, "height")
+
+fmt.Println(x)
+//fmt.Println(x["address"])
+}
