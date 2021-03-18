cadf95b747ce9f18c9b43401ce71b4bd66358315
Al Mamun Khan <67066348+Almamun1740@users.noreply.github.com>
2021-03-18 23:49:00 +0600

    recovery testing just fun don't panic

---
+++ recover.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+// func main() {
+// 	fmt.Println("start")
+// 	defer func() {
+// 		if err := recover(); err != nil {
+// 			log.Println("Error:", err)
+// 		}
+// 	}()
+// 	panic("something bad happend")
+// 	fmt.Println("end")
+// }
+
+func main() {
+	fmt.Println("start")
+	panicker()
+	fmt.Println("end")
+}
+func panicker() {
+	fmt.Println("about to panic")
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println("Error:", err)
+		}
+	}()
+	panic("something bad happened")
+	fmt.Println("done panicking")
+}
